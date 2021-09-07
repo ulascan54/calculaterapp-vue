dcff0391ce7f9dbfe94f371318ddc5a186f7c76f
Ulaş Can Demirbağ <ulascandemirbag@gmail.com>
2021-09-07 09:17:37 +0300

    tasarım tamamlandı

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template> 
+<Calculator />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Calculator from './components/Calculator.vue'
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Calculator,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="calculator">
+    <div class="display">0.0654654654</div>
+    <div class="btn">AC</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn operator">÷</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn operator">x</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn operator">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn operator">+</div>
+    <div class="btn zero">0</div>
+    <div class="btn">.</div>
+    <div class="btn operator">=</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.calculator{
+  margin: 0 auto;
+  width: 400px;
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4,1fr);
+  grid-auto-rows: minmax(50px,auto);
+}
+
+.display{
+  grid-column: 1 / 5;
+  background: #333;
+  color: white;
+}
+.zero{
+  grid-column: 1/3;
+}
+.btn{
+  background-color: #F2F2F2;
+  border: 1px solid #999;
+}
+.operator{
+  background-color: orange;
+  color: white;
+}
+</style>
