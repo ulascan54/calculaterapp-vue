88a09fd13060f4ebff1cc8f68e6a172bdfb519b1
Ulaş Can Demirbağ <ulascandemirbag@gmail.com>
2021-09-07 10:09:26 +0300

    proje tamamlandı

--- README.md
+++ README.md
@@ -1,24 +1,34 @@
 # calculatorapp-vue
 
+See [Project Reference](https://www.youtube.com/watch?v=m1_ih43p24s).
+<br>
+<br>
+![.](src/assets/1.png)
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,56 +1,128 @@
 <template>
   <div class="calculator">
-    <div class="display">0.0654654654</div>
-    <div class="btn">AC</div>
-    <div class="btn">+/-</div>
-    <div class="btn">%</div>
-    <div class="btn operator">÷</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
-    <div class="btn operator">x</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
-    <div class="btn operator">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
-    <div class="btn operator">+</div>
-    <div class="btn zero">0</div>
-    <div class="btn">.</div>
-    <div class="btn operator">=</div>
+    <div class="display">{{ current || 0 }}</div>
+    <div @click="clear" class="btn">AC</div>
+    <div @click="sign" class="btn">+/-</div>
+    <div @click="percent" class="btn">%</div>
+    <div @click="divide" class="btn operator">÷</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
+    <div @click="times" class="btn operator">x</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
+    <div @click="minus" class="btn operator">-</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
+    <div @click="add" class="btn operator">+</div>
+    <div @click="append('0')" class="btn zero">0</div>
+    <div @click="dot" class="btn">.</div>
+    <div @click="equal" class="btn operator equal">=</div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      previous: null,
+      current: "",
+      operator: null,
+      operatorClicked: false,
+    };
+  },
+  methods: {
+    clear() {
+      this.current = "";
+    },
+    sign() {
+      this.current =
+        this.current.charAt(0) === "-"
+          ? this.current.slice(1)
+          : `-${this.current}`;
+    },
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`;
+    },
+    append(number) {
+      if(this.operatorClicked){
+        this.current=""
+        this.operatorClicked=false
+      }
+      this.current = `${this.current}${number}`;
+    },
+    dot() {
+      if (this.current.indexOf(".") === -1) {
+        this.append(".");
+      }
+    },
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.setPrevious();
+    },
+    times() {
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
+    },
+    minus() {
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
+    },
+    add() {
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
+    },
+    equal() {
+      this.current= `${this.operator(parseFloat(this.current),parseFloat(this.previous))}`
+      this.previous=null
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.calculator{
+.calculator {
   margin: 0 auto;
   width: 400px;
   font-size: 40px;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  font-weight: lighter;
   display: grid;
-  grid-template-columns: repeat(4,1fr);
-  grid-auto-rows: minmax(50px,auto);
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
 }
 
-.display{
+.display {
+  border-radius: 15px 15px 0 0;
   grid-column: 1 / 5;
+  height: 60px;
+  line-height: 60px;
   background: #333;
   color: white;
 }
-.zero{
+.zero {
   grid-column: 1/3;
+  border-radius: 0 0 0 15px;
 }
-.btn{
-  background-color: #F2F2F2;
+.equal {
+  border-radius: 0 0 15px 0;
+}
+.btn {
+  background-color: #f2f2f2;
   border: 1px solid #999;
+  cursor: pointer;
 }
-.operator{
+.btn:active {
+  box-shadow: inset -1px 3px 8px 5px #999, 2px 5px 16px 0px;
+}
+.operator {
   background-color: orange;
   color: white;
 }
